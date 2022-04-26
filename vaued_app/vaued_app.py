88189b2362fc2322970d1cb46709b98ffcdc25b3
Ishara Dissanayake <thisismaduishara@gmail.com>
2022-04-26 22:39:24 +0530

    added classification endpoint

--- vaued_app/vaued_app.py
+++ vaued_app/vaued_app.py
@@ -30,6 +30,7 @@ def root():
     is_authenticated = False
     return render_template("Home.html")
 
+
 @app.route("/auth", methods=['POST'])
 @cross_origin(headers=['Content-Type'])
 def auth():
@@ -53,6 +54,7 @@ def admin_dashboard():
         return render_template("AdminDashboard.html")
     else:
         return render_template("Home.html", status="error")
+
 
 @app.route("/sign_out", methods=["GET"])
 @cross_origin(headers=['Content-Type'])
@@ -93,6 +95,36 @@ def mine_association_rule():
         result = association_rules_finder.get_rules_fp(movi_list)
     elif req_data["ALGO"] == 'APR':
         result = association_rules_finder.get_rules_ap(movi_list)
+
+
+@app.route("/MLService/classification", methods=['POST'])
+@cross_origin(headers=['Content-Type'])
+def classify():
+    req_data = request.json
+    if req_data['ALGO'] == 'Logistic Regression with OneVsRest and TF-IDF':
+        predictions = lrm.get_predictions(
+            req_data['PLOT'])
+        print(predictions)
+    elif req_data['ALGO'] == 'Linear SVC with OneVsRest and TF-IDF':
+        predictions = lsm.get_predictions(
+            req_data['PLOT'])
+        print(predictions)
+    elif req_data['ALGO'] == 'Random Forest Classifier with OneVsRest and TF-IDF':
+        # RFC model is too large, hence, it was removed before committing to git
+        predictions = ""  # rfm.get_predictions(req_data['PLOT'])
+        print(predictions)
+    elif req_data['ALGO'] == 'Perceptron with OneVsRest and TF-IDF':
+        predictions = pm.get_predictions(
+            req_data['PLOT'])
+        print(predictions)
+    elif req_data['ALGO'] == 'Decision Tree Regressor with OneVsRest and TF-IDF':
+        predictions = dtr.get_predictions(
+            req_data['PLOT'])
+        print(predictions)
+    else:
+        predictions = [['']]
+
+    return jsonify({'GENRES': ", ".join([x.capitalize() for x in predictions[0]])})
 
 
 if __name__ == "__main__":
