4af8085149027b78a55777b4f09671840f062485
Ishara Dissanayake <thisismaduishara@gmail.com>
2022-04-26 21:41:20 +0530

    added initial version of the flask server

--- vaued_app/vaued_app.py
+++ vaued_app/vaued_app.py
@@ -1,7 +1,9 @@
 import logging
-
+import re
 from flask import (
-    Flask, jsonify,
+    Flask,
+    jsonify,
+    request, render_template,
 )
 from flask_cors import cross_origin
 from flask_prometheus_metrics import register_metrics
@@ -9,25 +11,24 @@ from prometheus_client import make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
 
+import association_rules_finder
+import classification_logistic_reg_model as lrm
+import classification_linear_svm_model as lsm
+# import classification_random_forest_model as rfm
+import classification_perceptron_model as pm
+import classification_decision_tr_model as dtr
+
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
+is_authenticated = False
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 @cross_origin(headers=['Content-Type'])
 def root():
-    return "Welcome to the ML Recommendations Engine Backend"
-
-
-@app.route("/version")
-@cross_origin(headers=['Content-Type'])
-def version():
-    return jsonify(
-        {
-            "Server": "ML Recommendations Engine Backend",
-            "Version": "1.0.2"
-        }
-    )
+    global is_authenticated
+    is_authenticated = False
+    return render_template("Home.html")
 
 
 if __name__ == "__main__":
@@ -37,4 +38,4 @@ if __name__ == "__main__":
     # Plug metrics WSGI app to your main app with dispatcher
     dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
 
-    run_simple(hostname="0.0.0.0", port=9091, application=dispatcher)
+    run_simple(hostname="0.0.0.0", port=9091, application=dispatcher, use_debugger=True)
